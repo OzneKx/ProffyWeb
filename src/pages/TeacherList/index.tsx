add587ff08669b54b6f355b605c084f407bf2154
Kenzo De Albuquerque <kenzoalbuqk@gmail.com>
2020-08-05 17:46:18 -0300

    WEB - Proffy

--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -2,6 +2,8 @@ import React from "react";
 
 import PageHeader from "../../components/PageHeader";
 import TeacherItem from "../../components/TeacherItem";
+import Input from "../../components/Input";
+import Select from "../../components/Select";
 
 import "./styles.css";
 
@@ -10,24 +12,47 @@ function TeacherList() {
     <div id="page-teacher-list" className="container">
       <PageHeader title="Estes são os proffys disponíveis.">
         <form id="search-teachers">
-          <div className="input-block">
-            <label htmlFor="subject">Matéria</label>
-            <input type="text" id="subject" />
-          </div>
-
-          <div className="input-block">
-            <label htmlFor="week_day">Dia da Semana</label>
-            <input type="text" id="week_day" />
-          </div>
-
-          <div className="input-block">
-            <label htmlFor="time">Hora</label>
-            <input type="text" id="time" />
-          </div>
+          <Select
+            name="subject"
+            label="Matéria"
+            options={[
+              { value: "Artes", label: "Artes" },
+              { value: "Biologia", label: "Biologia" },
+              { value: "Educação Física", label: "Educação Física" },
+              { value: "Espanhol", label: "Espanhol" },
+              { value: "Filosofia", label: "Filosofia" },
+              { value: "Física", label: "Física" },
+              { value: "Geografia", label: "Geografia" },
+              { value: "História", label: "História" },
+              { value: "Inglês", label: "Inglês" },
+              { value: "Matemática", label: "Matemática" },
+              { value: "Música", label: "Música" },
+              { value: "Português", label: "Português" },
+              { value: "Química", label: "Química" },
+              { value: "Redação", label: "Redação" },
+              { value: "Sociologia", label: "Sociologia" },
+            ]}
+          />
+          <Select
+            name="week_day"
+            label="Doa da Semana"
+            options={[
+              { value: "0", label: "Domingo" },
+              { value: "1", label: "Segunda-feira" },
+              { value: "2", label: "Terça-feira" },
+              { value: "3", label: "Quarta-feira" },
+              { value: "4", label: "Quinta-feira" },
+              { value: "5", label: "Sexta-feira" },
+              { value: "6", label: "Sábado" },
+            ]}
+          />
+          <Input type="time" name="" label="Hora" />
         </form>
       </PageHeader>
 
       <main>
+        <TeacherItem />
+        <TeacherItem />
         <TeacherItem />
         <TeacherItem />
         <TeacherItem />
