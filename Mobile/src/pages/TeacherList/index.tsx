4de207e001fbce3de0d297552d5f27d356f177b0
Kenzo De Albuquerque <kenzoalbuqk@gmail.com>
2020-08-08 00:16:32 -0300

    Proffy - WEB

--- Mobile/src/pages/TeacherList/index.tsx
+++ Mobile/src/pages/TeacherList/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 
-import { View } from 'react-native';
+import { View, ScrollView} from 'react-native';
 
 import PageHeader from '../../components/PageHeader';
+import TeacherItem from '../../components/TeatcherItem';
 
 import styles from './styles';
 
@@ -10,6 +11,22 @@ function TeacherList() {
   return (
     <View style={styles.container}>
       <PageHeader title="Proffys disponíveis" />
+
+      <ScrollView 
+        style={styles.teacherList}
+        contentContainerStyle={{
+          paddingHorizontal: 16,
+          paddingBottom: 16,
+        }}
+      >
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+      </ScrollView>
     </View>
   );
 }
