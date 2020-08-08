92b2eef384d29918a089f9a38e201182196cfd22
Kenzo De Albuquerque <kenzoalbuqk@gmail.com>
2020-08-08 14:44:02 -0300

    Proffy - WEB

--- Mobile/src/pages/Favorites/index.tsx
+++ Mobile/src/pages/Favorites/index.tsx
@@ -1,13 +1,33 @@
-import React from 'react';
-
-import { View, ScrollView } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, ScrollView} from 'react-native';
+import { useFocusEffect } from '@react-navigation/native';
 
 import PageHeader from '../../components/PageHeader';
-import TeacherItem from '../../components/TeatcherItem';
+import TeacherItem, { Teacher } from '../../components/TeatcherItem';
+
+import AsyncStorage from '@react-native-community/async-storage';
 
 import styles from './styles';
 
 function Favorites() {
+  const [favorites, setFavorites] = useState([]);
+
+  function loadFavorites() {
+    AsyncStorage.getItem('favorites').then(response => {
+      if (response) {
+        const favoritedTeachers = JSON.parse(response);
+
+        setFavorites(favoritedTeachers);
+      }
+    });
+  }
+
+  useFocusEffect(
+    React.useCallback(() => {
+      loadFavorites();
+    }, [])
+  )
+
   return (
     <View style={styles.container}>
       <PageHeader title="Meus proffys disponíveis" />
@@ -19,13 +39,15 @@ function Favorites() {
           paddingBottom: 16,
         }}
       >
-        <TeacherItem />
-        <TeacherItem />
-        <TeacherItem />
-        <TeacherItem />
-        <TeacherItem />
-        <TeacherItem />
-        <TeacherItem />
+        {favorites.map((teacher: Teacher) => {
+          return (
+            <TeacherItem
+              key={teacher.id}
+              teacher={teacher}
+              favorited
+            />
+          )
+        })}
       </ScrollView>
 
     </View>
--- mobile/src/components/TeatcherItem/index.tsx
+++ mobile/src/components/TeatcherItem/index.tsx
@@ -1,56 +1,119 @@
-import React from "react";
-import { View, Image, Text } from "react-native";
+import React, { useState } from "react";
+import { View, Image, Text, Linking } from "react-native";
 import { RectButton } from "react-native-gesture-handler";
+import AsyncStorage from '@react-native-community/async-storage';
 
 import heartOutlineIcon from "../../assets/images/icons/heart-outline.png";
 import unfavoriteIcon from "../../assets/images/icons/unfavorite.png";
 import whatsappIcon from "../../assets/images/icons/whatsapp.png";
 
-import styles from "./styles";
+import api from "../../services/api";
 
-function TeacherItem() {
+import styles from './styles';
+
+export interface Teacher {
+  id: number;
+  avatar: string;
+  bio: string;
+  cost: number;
+  name: string;
+  subject: string;
+  whatsapp: string;
+}
+
+interface TeacherItemProps {
+  teacher: Teacher;
+  favorited: boolean;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher, favorited }) => {
+  const [isFavorited, setIsFavorited] = useState(favorited);
+
+  function handleLinkToWhatsapp(){
+    api.post('connections', {
+      user_id: teacher.id,
+    })
+
+    Linking.openURL(`whatsapp://send?phone=${teacher.whatsapp}`)
+  }
+
+  async function handleToggleFavorite() {
+    const favorites = await AsyncStorage.getItem('favorites');
+      
+      let favoritesArray = [];
+
+      if (favorites) {
+        favoritesArray = JSON.parse(favorites);
+      }
+
+    if (isFavorited) {
+      // Remover dos favoritos
+      const favoriteIndex = favoritesArray.findIndex((teacherItem: Teacher) => {
+        return teacherItem.id === teacher.id;
+      });
+      
+      favoritesArray.splice(favoriteIndex, 1);
+
+      setIsFavorited(false);
+    } else {
+        favoritesArray.push(teacher); 
+
+        setIsFavorited(true);
+    }
+
+    await AsyncStorage.setItem('favorites', JSON.stringify(favoritesArray));
+  }
+
   return (
     <View style={styles.container}>
       <View style={styles.profile}>
         <Image
           style={styles.avatar}
           source={{
-            uri:"https://avatars1.githubusercontent.com/u/64553441?s=460&u=e5457615f66b0a8976dcc012eaa45cdf7cdc3783&v=4"
+            uri: teacher.avatar
           }}
         />
 
           <View style={styles.profileInfo}>
-            <Text style={styles.name}>Kenzo De Albuquerque</Text>
-            <Text style={styles.subject}>Programação</Text>
+            <Text style={styles.name}>{teacher.name}</Text>
+            <Text style={styles.subject}>{teacher.subject}</Text>
           </View>
       </View>
 
       <Text style={styles.bio}>
         Estudante.
         {'\n'}{'\n'}
-        Aprendendo a programar.
+        {teacher.bio}
       </Text>
 
       <View style={styles.footer}>
         <Text style={styles.price}>
           Preço/Hora {'   '}
-          <Text style={styles.priceValue}>R$ 350,00</Text>
+          <Text style={styles.priceValue}>{teacher.cost}</Text>
         </Text>
         
         <View style={styles.buttonContainer}>
-          <RectButton style={{styles.favoriteButton, styles.favorited}}>
-            /* <Image source={heartOutlineIcon}/> */
-            <Image source={unfavoriteIcon}/>
-          </RectButton>
-        </View>
-
-        <View style={styles.buttonContainer}>
-          <RectButton style={styles.favoriteButton}>
+          <RectButton
+            onPress={handleToggleFavorite} 
+            style={[
+              styles.favoriteButton, 
+              isFavorited ? styles.favorited : {}, 
+            ]}
+          >
+            { isFavorited 
+              ? <Image source={unfavoriteIcon} /> 
+              : <Image source={heartOutlineIcon}/>
+            }
+           </RectButton>
+        
+          <RectButton 
+            onPress={handleLinkToWhatsapp} 
+            style={styles.contactButton}
+          >
             <Image source={whatsappIcon}/>
             <Text style={styles.contactButtonText}>Entrar em Contato</Text>
           </RectButton>
         </View>
-
       </View>
     </View>
   );
